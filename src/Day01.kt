d0f5d1c7c7288f289f61de92570fdedb6b653953
Kent <kent.yuan@Gmail.com>
2025-12-01 10:40:45 +0100

    Day 01 ✅

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,17 +7,46 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun List<String>.toDay01() = Day01(map { it[0] to it.drop(1).toInt() })
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) = input.toDay01().start().zeros
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    fun part2(input: List<String>) = input.toDay01().start2().zeros
+
+    chkTestInput(Part1, testInput, 3) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 6) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private data class Day01(val rotations: List<Pair<Char, Int>>) {
+    private var cur = 50
+    var zeros = 0 // the result
+
+    fun start() = apply {
+        rotations.forEach {
+            cur = when (it.first) {
+                'L' -> cur.rotateLeft(it.second)
+                'R' -> cur.rotateRight(it.second)
+                else -> error("Unexpected character: ${it.first}")
+            }
+            if (cur == 0) zeros++
+        }
+    }
+
+    private fun Int.rotateRight(x: Int): Int = (this + x) % 100
+    private fun Int.rotateLeft(x: Int): Int = this.rotateRight(100 - x % 100)
+
+    fun start2() = apply {
+        rotations.forEach { (dir, step) ->
+            zeros += step / 100
+            cur = when (dir) {
+                'L' -> cur.rotateLeft(step).also { if (it == 0 || (cur != 0 && it >= cur)) zeros++ }
+                'R' -> cur.rotateRight(step).also { if (it == 0 || (cur != 0 && it <= cur)) zeros++ }
+                else -> error("Unexpected character")
+            }
+        }
+    }
+
 }
