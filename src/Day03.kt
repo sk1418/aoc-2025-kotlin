85941e641cb2cca4567556b7fa6633ba079a4eb3
Kent <kent.yuan@Gmail.com>
2025-12-03 09:50:57 +0100

    Day 03 ✅

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,17 +7,39 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun List<String>.toDay03() = Day03(map { it.toList().map { it.digitToInt() } })
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>): Long = input.toDay03().sumLargest()
+    fun part2(input: List<String>): Long = input.toDay03().sumLargest2()
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 357) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 3121910778619) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private data class Day03(val allDigits: List<List<Int>>) {
+    fun sumLargest() = allDigits.sumOf { digits ->
+        val (idx, v1) = digits.dropLast(1).withIndex().maxBy { it.value }
+        val v2 = digits.drop(idx + 1).max()
+        v1 * 10L + v2
+    }
+
+    fun sumLargest2(): Long {
+        val len = allDigits.first().size
+        return allDigits.sumOf { digits ->
+            var remaining = 12
+            val sorted = digits.withIndex().sortedWith(compareByDescending<IndexedValue<Int>> { it.value }.thenBy { it.index }).toMutableList()
+            var num = 0L
+            while (remaining > 0) {
+                val (idx, v) = sorted.first { it.index <= len - remaining }
+                num = num * 10 + v
+                sorted.removeAll { it.index <= idx }
+                remaining--
+            }
+            num
+        }
+    }
+
 }
