c1c806305bd8c7984dcb53aee96a4d7a56a8b05a
Kent <kent.yuan@Gmail.com>
2025-12-04 10:30:41 +0100

    Day 04 ✅

--- src/Day04.kt
+++ src/Day04.kt
@@ -7,17 +7,39 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
+    fun List<String>.toMatrix(): MatrixDay04 {
+        val points = buildMap {
+            forEachIndexed { y, line -> line.forEachIndexed { x, c -> put(x to y, c) } }
+        }
+        return MatrixDay04(this[0].lastIndex, this.lastIndex, MutableNotNullMap(points.toMutableMap()))
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) = input.toMatrix().findRolls()
+    fun part2(input: List<String>) = input.toMatrix().findRemovable()
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 13) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 43) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+class MatrixDay04(maxX: Int, maxY: Int, override val points: MutableNotNullMap<Point, Char>) : Matrix<Char>(maxX, maxY, points) {
+
+    private fun accessibleRolls() = points.filter { (p, c) -> c == '@' && p.allAroundIn().count { points[it] == '@' } < 4 }.keys
+    fun findRolls() = accessibleRolls().size
+
+    fun findRemovable(): Int {
+        var stepCount = Int.MAX_VALUE
+        var result = 0
+        while (stepCount > 0) {
+            stepCount == 0
+            accessibleRolls().let {
+                stepCount = it.size
+                result += stepCount
+                points.putAll(it.map { it to '.' })
+            }
+        }
+        return result
+    }
 }
