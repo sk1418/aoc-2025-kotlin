1790a1883a735a50139efdb2ef981a6c720da5e5
Kent <kent.yuan@Gmail.com>
2025-12-09 09:17:34 +0100

    Day 09 ✅

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,4 +1,7 @@
 import utils.*
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 // https://adventofcode.com/2025/day/9
 fun main() {
@@ -7,17 +10,50 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun List<String>.toDay09() = Day09(map { line -> line.split(",").let { (x, y) -> x.toInt() to y.toInt() } })
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>): Long = input.toDay09().largestArea()
+    fun part2(input: List<String>): Long = input.toDay09().largestAreaByRedAndGreen()
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 50) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 24) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private class Day09(val redPoints: List<Point>) {
+    val cornerPairs = buildList {
+        for (i in redPoints.indices) {
+            for (j in i + 1 until redPoints.size) {
+                add(redPoints[i] to redPoints[j])
+            }
+        }
+    }
+
+    private val area: Pair<Point, Point>.() -> Long = {
+        val (a, b) = this
+        (abs(a.first - b.first) + 1L) * (abs(b.second - a.second) + 1L)
+    }
+
+    fun largestArea(): Long = cornerPairs.maxOf { it.area() }
+
+    fun largestAreaByRedAndGreen(): Long {
+        fun Pair<Point, Point>.toSortedEdgePoints(): Pair<Point, Point> {
+            val (p1, p2) = this
+            val point1 = min(p1.first, p2.first) to min(p1.second, p2.second)
+            val point2 = max(p1.first, p2.first) to max(p1.second, p2.second)
+            return point1 to point2
+        }
+
+        val polygonEdges = (redPoints + redPoints.first())
+            .zipWithNext()
+            .map { it.toSortedEdgePoints() }
+
+        return cornerPairs.map { it.toSortedEdgePoints() }.filter { (p1, p2) ->
+            polygonEdges.none { (e1, e2) ->
+                p1.first < e2.first && p2.first > e1.first && p1.second < e2.second && p2.second > e1.second
+            }
+        }.maxOf { it.area() }
+    }
 }
