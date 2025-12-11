1565a53cc5f622873e0a852b0285a2d25b6c9f5b
Kent <kent.yuan@Gmail.com>
2025-12-11 23:52:33 +0100

    Day 11 ✅

--- src/Day11.kt
+++ src/Day11.kt
@@ -5,19 +5,36 @@ fun main() {
     val today = "Day11"
 
     val input = readInput(today)
-    val testInput = readTestInput(today)
+    val testInput1 = readTestInput("${today}-part1")
+    val testInput2 = readTestInput("${today}-part2")
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>): Long = Day11(input).countPaths("you")
+    fun part2(input: List<String>): Long = Day11(input).countPaths("svr", setOf("fft", "dac"))
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
 
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput1, 5) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput2, 2) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+private data class Day11(val lines: List<String>) {
+    private val splitRe = Regex(""":? """)
+    val routes = lines.associate { line ->
+        line.split(splitRe).let {devList-> devList[0] to devList.drop(1) }
+    }.toMutableNotNullMap()
+
+    val cache = mutableMapOf<Pair<String, Set<String>>, Long>()
+
+    fun countPaths(current: String, required: Set<String> = setOf(), reachedKeyNode: Set<String> = setOf()): Long =
+        cache.getOrPut(current to reachedKeyNode) {
+            if (current == "out") {
+                if (reachedKeyNode == required) 1L else 0L
+            } else {
+                routes[current].sumOf { next ->
+                    countPaths(next, required, if (next in required) reachedKeyNode + next else reachedKeyNode)
+                }
+            }
+        }
 }
