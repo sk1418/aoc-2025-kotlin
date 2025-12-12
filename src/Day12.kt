2dbbf84c24e30211c2b93b9c4b87cb05a4d3c70b
Kent <kent.yuan@Gmail.com>
2025-12-12 11:26:44 +0100

    Day 12 ✅

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,4 +1,7 @@
-import utils.*
+import utils.Part1
+import utils.readInput
+import utils.readTestInput
+import utils.solve
 
 // https://adventofcode.com/2025/day/12
 fun main() {
@@ -7,17 +10,9 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
-
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    // 🤓 rotating the shapes in the real input makes no difference (but test input does!), so it becomes a fairly easy challenge and skipping the test
+    fun part1(input: List<String>) = input.takeLastWhile { it.isNotBlank() }.map { it.split(Regex("""\D{1,2}""")).map { it.toInt() } }
+        .count { nums -> nums.drop(2).sum() <= nums[0] * nums[1] / 9 }
+    //  chkTestInput(Part1, testInput, 2) { part1(it) }
     solve(Part1, input) { part1(it) }
-
-    chkTestInput(Part2, testInput, 0) { part2(it) }
-    solve(Part2, input) { part2(it) }
 }
